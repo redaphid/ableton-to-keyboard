0022ac400f73dbc970ab519b523231d84aae909c
redaphid <iam@aaronherres.com>
2021-09-17 20:04:38 -0700

    alright. the loop is working

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 An extremely hacky way to use the ableton push 1 as a macro pad.
 It uses [virtual-hid-tcp](https://github.com/nmelihsensoy/virtual-hid-tcp) to fake keyboard events
 then [push-wrapper](https://github.com/crosslandwa/push-wrapper) to read from the push
+
+
+nc 127.0.0.1 8080 30115
---
+++ server.js
@@ -0,0 +1,17 @@
+const net = require('net');
+
+const port = 8080;
+const host = 'localhost';
+
+// Create a new socket
+const socket = new net.Socket();
+
+// Establish a TCP connection
+socket.connect(port, host);
+
+const virtualKeyboardRequest = '30115'
+socket.on('connect', () => {
+    socket.write(virtualKeyboardRequest)
+  console.log(`Established a TCP connection with ${host}:${port}`);
+  socket.destroy();
+});
