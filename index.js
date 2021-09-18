695a59653e0e3798d57779437e0ea4b86e3553c0
redaphid <iam@aaronherres.com>
2021-09-17 19:42:36 -0700

    looks like my only option might be webmidi

--- index.js
+++ index.js
@@ -2,15 +2,24 @@ const pushWrapper = require('push-wrapper')
 const midi = require('midi')
 
 const input = new midi.Input()
-console.log(input.getPortCount())
-const push = pushWrapper.push()
-push.onMidiToHardware(console.log)
-console.log('huh?')
-const gridSelectButton = push.gridSelectButtons()[0]
-gridSelectButton.onPressed(()=>{
-    console.log('pressed!')
+const numPorts = input.getPortCount()
+//Just connect the Ableton after everything else. What could go wrong?
+
+// for(var i = 0; i < numPorts; i++){
+//     input.openPort(i)
+// }
+input.openPort(1)
+input.on('message', (deltaTime, message)=>{
+    console.log(message)
 })
-push.clearLCD()
-push.button('TapTempo').ledOn() 
-process.stdin.resume()
-// push.button('TapTempo').ledOn() // prints [176, 3, 4] to console
+// const push = pushWrapper.push()
+// push.onMidiToHardware(console.log)
+// console.log('huh?')
+// const gridSelectButton = push.gridSelectButtons()[0]
+// gridSelectButton.onPressed(()=>{
+//     console.log('pressed!')
+// })
+// push.clearLCD()
+// push.button('TapTempo').ledOn() 
+// process.stdin.resume()
+// // push.button('TapTempo').ledOn() // prints [176, 3, 4] to console
